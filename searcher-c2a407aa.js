d01328b8ac01591da36f3ed0a0446f694e90fc98
xymaxim <xymaxim@users.noreply.github.com>
2026-02-24 10:37:11 +0000

    deploy: eb67f7caf02bbb198bf54173be7a512f852933c6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c12704c7.js',
+                path_to_root + 'searchindex-9571e2e5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
