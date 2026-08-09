50f7e1c14b6052e72eaa1c91de261231c57bd661
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2026-08-09 16:38:43 +0300

    refactor: intercept initialization requests

--- internal/app/web/src/player.js
+++ internal/app/web/src/player.js
@@ -1,8 +1,32 @@
 import { MediaPlayer } from 'https://cdn.dashjs.org/v5.2.0/modern/esm/dash.all.min.js';
 
 export function createPlayer(video, mpdURL) {
-  const player = MediaPlayer().create();
-  player.initialize(video, mpdURL, true);
+    const player = MediaPlayer().create();
+
+    // Extend the RequestModifier to intercept initialization requests
+    player.addRequestInterceptor(function (request) {
+        if (request.type === 'InitializationSegment') {
+            request.url = 'data:application/octet-stream;base64,';
+            request.withCredentials = false;
+        }
+        return Promise.resolve(request);
+    });
+
+    // // Clear out player tracking that depends on distinct init boundaries
+    // player.updateSettings({
+    //     streaming: {
+    //         buffer: {
+    //             fastSwitchEnabled: false // Prevents track switches from demanding unique init chunks
+    //         }
+    //     }
+    // });
+
+    player.on(MediaPlayer.events.ERROR, function (event) {
+       console.error("Player Error:", event.message, "Type:", event.error);
+    });
+    
+    player.initialize(video, mpdURL, true);
+    
   return player;
 }
 
